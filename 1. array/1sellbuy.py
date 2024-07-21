aa7d91202b995f96b1248dedd175e2591ab69e2c
Nga Ha Thanh <75192834+chaugaialberteinstein@users.noreply.github.com>
2024-07-21 23:30:18 +0700

    Update 1sellbuy.py

--- 1. array/1sellbuy.py
+++ 1. array/1sellbuy.py
@@ -1,3 +1,4 @@
+# Problem statement : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
         min_price = prices[0]  # Initialize minimum price
